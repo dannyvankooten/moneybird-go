0dd746cf7affc4459ad928171f66e4ec9cb665ee
Danny van Kooten <dannyvankooten@gmail.com>
2017-03-09 20:26:24 +0100

    decode errors returned by API, #2

--- response.go
+++ response.go
@@ -10,6 +10,29 @@ type Response struct {
 	*http.Response
 }
 
+// APIError holds data for a MoneyBird API error
+type APIError struct {
+	Err      string              `json:"error,omitempty"`
+	Errs     map[string][]string `json:"errors,omitempty"`
+	Symbolic map[string]string   `json:"symbolic,omitempty"`
+}
+
+func (e *APIError) Error() string {
+	return e.Err
+}
+
+func (res *Response) error() error {
+	apiErr := &APIError{}
+
+	// try to decode into APIError struct
+	err := json.NewDecoder(res.Body).Decode(apiErr)
+	if err != nil {
+		return err
+	}
+
+	return apiErr
+}
+
 func (res *Response) contact() (*Contact, error) {
 	var contact *Contact
 	err := json.NewDecoder(res.Body).Decode(&contact)
