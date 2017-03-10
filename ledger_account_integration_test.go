14e7b0f5e0d38336c51de5d8ad530cb6a12047d4
Danny van Kooten <dannyvankooten@gmail.com>
2017-03-10 11:02:54 +0100

    add missing ledger account integration tests

--- ledger_account_integration_test.go
+++ ledger_account_integration_test.go
@@ -11,3 +11,48 @@ func TestLedgerAccountGatewayAll(t *testing.T) {
 		t.Error(err)
 	}
 }
+
+func TestLedgerAccountGatewayCRUD(t *testing.T) {
+	gateway := testClient.LedgerAccount()
+
+	// create
+	acct, err := gateway.Create(&LedgerAccount{
+		Name:        "Server costs",
+		AccountType: "expenses",
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if acct.AccountType != "expenses" {
+		t.Errorf("LedgerAccount.Create(): invalid account type %#v", acct.AccountType)
+	}
+
+	// read
+	acct, err = gateway.Get(acct.ID)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if acct.Name != "Server costs" {
+		t.Errorf("LedgerAccount.Get(): invalid account name %#v", acct.Name)
+	}
+
+	// update
+	acct.Name = "Support costs"
+	acct, err = gateway.Update(acct)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if acct.Name != "Support costs" {
+		t.Errorf("LedgerAccount.Update(): invalid account name %#v", acct.Name)
+	}
+
+	// delete
+	err = gateway.Delete(acct)
+	if err != nil {
+		t.Error(err)
+	}
+}
