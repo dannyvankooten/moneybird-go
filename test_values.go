f89933d59ebf22f53ff5e1b43b69caeda15605c2
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2022-04-05 09:50:13 +0200

    check env vars before running tests

--- test_values.go
+++ test_values.go
@@ -3,10 +3,25 @@ package moneybird
 import (
 	"net/http"
 	"os"
+	"fmt"
 )
 
-var testClient = &Client{
-	Token:            os.Getenv("MONEYBIRD_TEST_TOKEN"),
-	AdministrationID: os.Getenv("MONEYBIRD_TEST_ADMINISTRATION_ID"),
-	HTTPClient:       &http.Client{},
+var testClient *Client
+
+func init() {
+	if os.Getenv("MONEYBIRD_TEST_TOKEN") == "" {
+		fmt.Printf("Environment value MONEYBIRD_TEST_TOKEN not set\n")
+		os.Exit(1)
+	}
+
+	if os.Getenv("MONEYBIRD_TEST_ADMINISTRATION_ID") == "" {
+		fmt.Printf("Environment value MONEYBIRD_TEST_ADMINISTRATION_ID not set\n")
+		os.Exit(1)
+	}
+
+	testClient = &Client{
+		Token:            os.Getenv("MONEYBIRD_TEST_TOKEN"),
+		AdministrationID: os.Getenv("MONEYBIRD_TEST_ADMINISTRATION_ID"),
+		HTTPClient:       &http.Client{},
+	}
 }
